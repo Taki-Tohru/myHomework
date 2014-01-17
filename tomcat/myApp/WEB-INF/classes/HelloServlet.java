b203421dedd3c4209cfcc38376d18c3a2c44b3a0
Li MY <limy790@gmail.com>
2014-01-17 23:13:19 +0800

    add an servlet example

---
+++ tomcat/myApp/WEB-INF/classes/HelloServlet.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+		throws ServletException, IOException
+	{
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+
+		pw.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">");
+		pw.println("<head>");
+		pw.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">");
+		pw.println("<!-- The Servlet expression tags interpolate script variables into the HTML -->");
+		pw.println("<title>Hello, world!</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<h1>Hello, world!</h1>");
+		pw.println("</body>");
+		pw.close();
+	}
+	public HelloServlet() {}
+}
---
+++ tomcat/myApp/readme.txt
@@ -0,0 +1,2 @@
+把myapp文件夾放在tomcat/webapps下。
+http://localhost:8080/myApp/hello.do
